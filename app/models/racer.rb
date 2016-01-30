704d959b98311f21e084e73c2e83a048b7d44ef2
Song Yangyu <flyfy1@gmail.com>
2016-01-31 00:56:48 +0800

    question 6 done

--- app/models/racer.rb
+++ app/models/racer.rb
@@ -1,5 +1,7 @@
 class Racer
   COLUMNS = [:number, :first_name, :last_name, :gender, :group, :secs]
+  COLUMN_TYPES = {number: Integer, secs: Integer}
+
   attr_accessor :id, *COLUMNS
 
   def self.mongo_client
@@ -16,9 +18,9 @@ class Racer
   end
 
   def self.find id
-    Rails.logger.debug {"finding racer ##{id}"}
+    Rails.logger.debug {"finding racer id: #{id}"}
 
-    doc = collection.find(_id: id).first
+    doc = collection.find(_id: BSON.ObjectId(id)).first
     doc.nil? ? nil : Racer.new(doc)
   end
 
@@ -33,6 +35,23 @@ class Racer
     r.successful?
   end
 
+  def update(params)
+    return false if params.nil?
+
+    params.slice!(*COLUMNS)
+
+    s = self.class.collection.find(_id:BSON.ObjectId(@id)).update_one('$set': params)
+
+    if s.successful?
+      COLUMNS.each do |sym|
+        v = get_value(sym, params[sym])
+        instance_variable_set("@#{sym}".to_sym, v) if v
+      end
+    end
+
+    s.successful?
+  end
+
   def initialize(params={})
     @id = params[:_id].nil? ? params[:id] : params[:_id].to_s
 
@@ -42,5 +61,11 @@ class Racer
     end
     @number = params[:number].to_i
     @secs = params[:secs].to_i
+  end
+
+  private
+  def get_value(sym, v)
+    type = COLUMN_TYPES[sym]
+    if v and type == Integer then v.to_i else v end
   end
 end
