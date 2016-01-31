9fccafdd3125673084af84f3579485a371de93c5
Song Yangyu <flyfy1@gmail.com>
2016-01-31 14:26:51 +0800

    done q5

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,7 +1,17 @@
 class Place
+  attr_accessor :id, :formatted_address, :location, :address_components
+
   class << self
     def mongo_client; Mongoid::Clients.default; end
     def collection; self.mongo_client[:places]; end
     def load_all(json_file); self.collection.insert_many JSON.parse(json_file.read); end
+  end
+
+
+  def initialize(params)
+    @id = params[:_id].to_s
+    @formatted_address = params[:formatted_address]
+    @location = Point.new params[:geometry][:geolocation]
+    @address_components = params[:address_components].map {|c| AddressComponent.new(c)}
   end
 end
